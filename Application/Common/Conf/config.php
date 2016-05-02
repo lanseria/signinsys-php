e66bf0929013ff7a3e0a91cb9ccf9530a57e4a1e
Lanseria <zhangchao564265135@hotmail.com>
2016-05-02 11:26:26 +0800

    Update config.php

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -7,7 +7,7 @@ return array(
     'DB_HOST'               =>  '127.0.0.1', // 服务器地址
     'DB_NAME'               =>  'mm',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  'zc564265135',          // 密码
+    'DB_PWD'                =>  'YOUR PASS',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'm_',    // 数据库表前缀
     'DB_PARAMS'          	=>  array(), // 数据库连接参数
