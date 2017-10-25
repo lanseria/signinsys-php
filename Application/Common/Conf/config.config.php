f0a187da089f6959caa624ae59e5a453faa24c6f
lanseria <zhangchao564265135@hotmail.com>
2017-10-25 12:14:53 +0800

    update

--- Application/Common/Conf/config.config.php
+++ Application/Common/Conf/config.config.php
@@ -5,11 +5,11 @@ return array(
     //'URL_HTML_SUFFIX'       =>  'shtml',
     'DB_TYPE'               =>  'mysql',     // 数据库类型
     'DB_HOST'               =>  '127.0.0.1', // 服务器地址
-    'DB_NAME'               =>  'mm',          // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  'zc564265135',          // 密码
+    'DB_NAME'               =>  '',          // 数据库名
+    'DB_USER'               =>  '',      // 用户名
+    'DB_PWD'                =>  '',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
-    'DB_PREFIX'             =>  'm_',    // 数据库表前缀
+    'DB_PREFIX'             =>  '',    // 数据库表前缀
     'DB_PARAMS'          	=>  array(), // 数据库连接参数
     'DB_DEBUG'  			=>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
     'DB_FIELDS_CACHE'       =>  true,        // 启用字段缓存
